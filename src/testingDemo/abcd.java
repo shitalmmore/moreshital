ee1c21103b1e8fce6742d7f3d3085ff97f5ddbc3
shitalmmore <shitalmarotimoregmail.com>
2023-07-12 12:16:02 +0530

    user defined command

---
+++ src/testingDemo/abcd.java
@@ -0,0 +1,10 @@
+package testingDemo;
+
+public class abcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/testingDemo/newabcd.java
@@ -0,0 +1,10 @@
+package testingDemo;
+
+public class newabcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
